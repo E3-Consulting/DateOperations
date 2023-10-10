dfa11075ba27dfdc81e0f2a411c17ec2ad7a47fe
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2023-10-10 15:56:45 -0400

    Add project files.

---
+++ DateOperations/DateOps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateOperations
+{
+    public class DateOps
+    {
+        public int DateDiffDays(string dateFormat, string from, string to)
+        {
+            DateTime fromDate = DateTime.ParseExact(from, dateFormat, CultureInfo.InvariantCulture);
+            DateTime toDate = DateTime.ParseExact(to, dateFormat, CultureInfo.InvariantCulture);
+            TimeSpan dateDiff = fromDate - toDate;
+            return Math.Abs(dateDiff.Days);
+        }
+    }
+}
